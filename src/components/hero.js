6611eb0d407955fef8897e007fb8c2ac1f41883b
domogami <domogami@gmail.com>
2022-06-25 00:09:19 -0700

    🔁 Added animation

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import "../styles/components/all.css"
+import EricTesseract from "../assets/EricTesseract.webm"
+
 export default function Hero() {
   return (
     <div className='hero-container'>
+      <div className='video-container'>
+        <video autostart autoPlay muted loop id="myVideo">
+          <source src={EricTesseract} type="video/webm" />
+        </video>
+      </div>
       <div className="hero-text">
         <h2>Eric Jaszkowiak</h2>
         <h3>Electro-Mechanical Engineer</h3>
@@ -15,6 +22,7 @@ export default function Hero() {
           <li><a href="mailto:ejaszkowiak@gmail.com">Contact</a></li>
         </ul>
       </div>
+
     </div>
   )
 }
