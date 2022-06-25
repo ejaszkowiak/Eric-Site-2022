4cd00e67ac4e24dfea09da456607c2f62240e66a
domogami <domogami@gmail.com>
2022-06-25 11:52:54 -0700

    📝 About section done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Eric-Site-2.0
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/5539f237-7fa6-4ebe-ba15-2bb7461b5a09/deploy-status)](https://app.netlify.com/sites/ericjaszkowiak/deploys)
+
+Eric's [new site](https://ericjaszkowiak.netlify.app)
--- src/components/about.js
+++ src/components/about.js
@@ -4,9 +4,16 @@ import "../styles/components/all.css"
 export default function About() {
   return (
     <div className='about-container'>
-      <h1>About</h1>
-      <div className='fancy-rectangle' />
-      <p>I am an Electro-Mechanical Engineering student at Cal Poly Pomona, CA. I have experience as a Test Engineer Intern at Volvo with electric vehicles (EV), and Joby Aviation with electric vertical take off and landing aircraft (eVTOL). Graduating in December, 2023.</p>
+      <div className='about-text'>
+        <h1>About</h1>
+        <div className='fancy-rectangle' />
+        <p>
+          I am an Electro-Mechanical Engineering student at Cal Poly Pomona, CA. I have experience as a Test Engineer Intern at Volvo with electric vehicles (EV), and Joby Aviation with electric vertical take off and landing aircraft (eVTOL). Graduating in December, 2023.
+        </p>
+      </div>
+      <div className='button-container'>
+        <button>R&eacute;sum&eacute;</button>
+      </div>
     </div>
   )
 }
