27eecfcdaca739bc02ea739b30a87779d764330a
domogami <domogami@gmail.com>
2022-07-24 23:37:13 -0700

    🪴 Lots of small visual tweaks

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div className='about-container'>
       <div className='about-text'>
-        <h1>About</h1>
+        <h1>About Me</h1>
         <div className='fancy-rectangle' />
         <p>
           I am an Electro-Mechanical Engineering student at Cal Poly Pomona, CA. I have experience as a Test Engineer Intern at Volvo with electric vehicles (EV), and Joby Aviation with electric vertical take off and landing aircraft (eVTOL). Graduating in December, 2023.
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import "../styles/components/all.css"
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedinIn} from "@fortawesome/free-brands-svg-icons"
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Contact() {
+
+  library.add(faCoffee, faGithub, faLinkedinIn);
   return (
     <div className='contact-container'>
       <h2>Want to get in touch?</h2>  
@@ -9,10 +17,10 @@ export default function Contact() {
         <a href="mailto:ejaszkowiak@gmail.com">Let's Talk</a>
       </div>
       <div className='links'>
-        <a href="https://www.linkedin.com/in/eric-jaszkowiak/"><i class="fa-brands fa-linkedin-in"></i></a>
-       <a href="https://github.com/ejaszkowiak"><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/eric-jaszkowiak/"><FontAwesomeIcon icon={["fab", "linkedin-in"]}/></a>
+       <a href="https://github.com/ejaszkowiak"><FontAwesomeIcon icon={["fab", "github"]}/></a>
       </div>
-      <p className='credit'>Made with <a href="https://github.com/domogami"><i class="fa-solid fa-coffee"/></a>  by <a href="https://dominicklee.net">Dom</a></p>
+      <p className='credit'>Made with <a href="https://github.com/domogami"><FontAwesomeIcon icon="coffee"/></a>by <a href="https://dominicklee.net"><b>Dom</b></a></p>
     </div>
   )
 }
